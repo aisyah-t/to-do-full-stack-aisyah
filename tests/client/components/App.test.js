7b0468ff3c04473d78548aca8d70da5cf6536a05
Rose <62679021+rose-mead@users.noreply.github.com>
2020-07-08 10:52:44 +1200

    Add test for App conditional rendering

--- tests/client/components/App.test.js
+++ tests/client/components/App.test.js
@@ -2,9 +2,14 @@ import React from 'react'
 import {App} from '../../../client/components/App'
 import { shallow } from 'enzyme'
 
-test('App renders correctly', () => {
+test('App renders correctly on list view', () => {
   const wrapper = shallow(<App dispatch={() => {}} pageView={'list'}/>)
-  const actual = wrapper.find('h1')
+  const actual = wrapper.find('Connect(Form)')
   expect(actual).toHaveLength(1)
-  // console.log(wrapper.debug())
+})
+
+test('App renders correctly on edit view', () => {
+  const wrapper = shallow(<App dispatch={() => {}} pageView={'edit'}/>)
+  const actual = wrapper.find('Connect(Form)')
+  expect(actual).toHaveLength(0)
 })
